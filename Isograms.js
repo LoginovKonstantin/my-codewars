3865b162c76836d0fb22dcdabfcd425708413f4d
Konstantin Loginov <konstantin.loginov@onetwotrip.com>
2020-05-29 15:05:04 +0300

    Refactor old task

--- Isograms.js
+++ Isograms.js
@@ -1,12 +1,3 @@
 function isIsogram(str){
-    let lowStr = str.toLowerCase();
-    let flag = true;
-    for(let i = 0; i < lowStr.length; i ++) {
-        var a = 0;
-        for(let j = 0; j < lowStr.length; j ++) {
-            if(lowStr[i] === lowStr[j]) a++;
-        }
-        if(a > 1) flag = false;
-    }
-    return flag;
+    return new Set([...str.toLowerCase()]).size === str.length
 }
